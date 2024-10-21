85c218d2a5c1a6419ce81c52270e41dff6d8d29b
yamilobt8 <yamilo12345678910@gmail.com>
2024-10-21 17:59:07 +0100

    adding the templates folder and the layout

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('welcome to my website')
+
+def home(request):
+    return render(request, 'core/layout.html')
